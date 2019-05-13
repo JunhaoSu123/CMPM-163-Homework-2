a63ced9d6b10bfce2272ddcfe723c5e031088fc6
Junhao Su <jusu@ucsc.edu>
2019-05-12 19:02:10 -0700

    HW2

---
+++ Scripts/HeightChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightChange : MonoBehaviour
+{
+    public GameObject HillScale;
+    public float ScaleZ = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HillScale.transform.localScale = new Vector3(100, 100, ScaleZ);
+    }
+
+    public void scaleChange(float newScaleZ)
+    {
+        ScaleZ = newScaleZ;
+    }
+}
